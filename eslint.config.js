cc93975b431ac0adbbd931dbe8ba7c8454359358
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-07-11 20:56:23 +0300

    update_server

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,23 @@ import globals from "globals";
 
 export default [
   {
-
-    languageOption: {globals: globals.browser},
-    rules:{
-        indent: ["error", 4],
-        "quotes": ["error", "single"],
-        "semi": ["error", "always"],
-        "no-console": "off"
+    ignores: ["src/public/**"], // שימוש במאפיין ignores במקום ignorePatterns
+    languageOptions: { globals: globals.browser },
+    rules: {
+      indent: ['error', 4],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
     }
-
-}
+  },
+  {
+    ignores: ["src/**"], // שימוש במאפיין ignores במקום ignorePatterns
+    languageOptions: { globals: globals.browser },
+    rules: {
+      indent: ['error', 4],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+    }
+  },
 ];
+
+
