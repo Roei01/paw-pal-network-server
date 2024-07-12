6c9ab15203b5cd89c646553d093c51c9249480f5
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-07-12 05:38:03 +0300

    update login and register server

--- src/server.js
+++ src/server.js
@@ -64,7 +64,6 @@ app.post('/login',async (req, res) => {
   }
 
   const isMatch = await bcrypt.compare(password, user.password);
-
   if (!isMatch) {
     return res.status(400).send('Invalid credentials');
   }
