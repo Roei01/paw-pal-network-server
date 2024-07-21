21426c717fd06be49a7552c418b761beb4f3a9ac
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-07-21 22:41:40 +0300

    update port 10000

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 
 // CORS options
