cc48a8a51f5b4442211941df5c05b04ff623bfbb
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-07-21 08:42:11 +0300

    update localhost:27017 to be localhost:27017 and not cloud (for test)

--- src/server.js
+++ src/server.js
@@ -10,13 +10,12 @@ const port = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: 'http://localhost:4200', // Adjust this to match your Angular app's URL
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
 };
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
-
 
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/pawpal-network')
@@ -40,7 +39,7 @@ const UserSchema = new mongoose.Schema({
 const User = mongoose.model('User', UserSchema);
 
 // Routes
-app.post('/register', async (req, res) => {
+app.post('/api/register', async (req, res) => {
   const { username, firstName, lastName, email, password, dateOfBirth } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = new User({
@@ -65,7 +64,7 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post('/api/login', async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
 
@@ -86,7 +85,7 @@ app.post('/login', async (req, res) => {
   res.json({ token });
 });
 
-app.get('/profile', authenticateToken, async (req, res) => {
+app.get('/api/profile', authenticateToken, async (req, res) => {
   try {
     const user = await User.findById(req.user.id);
     res.json(user);
@@ -97,7 +96,7 @@ app.get('/profile', authenticateToken, async (req, res) => {
 
 // Middleware to authenticate token
 function authenticateToken(req, res, next) {
-  const token = req.header('Authorization').replace('Bearer ', '');
+  const token = req.header('Authorization')?.replace('Bearer ', '');
 
   if (!token) {
     return res.status(401).send('Access denied');
@@ -105,7 +104,7 @@ function authenticateToken(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, 'secretKey');
-    req.user = decoded; // יצירת עותק של req במקום לשנות אותו ישירות
+    req.user = decoded;
     next();
   } catch (err) {
     res.status(400).send('Invalid token');
@@ -116,4 +115,4 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-export default app; // הוספת שורת הייצוא
+export default app;
