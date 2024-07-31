fe11592b3c695297b1d0aa04f319ca600854a45d
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-07-31 19:16:03 +0300

    update

--- src/server.js
+++ src/server.js
@@ -58,6 +58,7 @@ const PostSchema = new mongoose.Schema({
   description: { type: String, required: true },
   image: { type: String },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  authorName: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   shares: [{
