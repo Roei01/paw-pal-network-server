44b72a088ee5c65e9b4777cc13a92f577f593456
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-08-02 07:02:27 +0300

    /////

--- src/server.js
+++ src/server.js
@@ -349,25 +349,28 @@ app.post('/posts/:id/share', authenticateToken, async (req, res) => {
 app.post('/posts/:id/save', authenticateToken, async (req, res) => {
   const { id } = req.params;
 
-  try {
+try {
     const post = await Post.findById(id);
+    const user = await User.findById(req.user.id);
     if (!post) {
       return res.status(404).send('Post not found');
     }
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
 
-    // הוספת מזהה הפוסט לרשימת השמירות של המשתמש
-    const user = await User.findById(req.user.id);
-    if (!user.savedPosts.includes(id)) {
-      user.savedPosts.push(id);
+    const userId = req.user.id;
+
+    if (user.savedPosts.includes(id)) {
+      user.savedPosts.pull(id);
     } else {
-      return res.status(400).send('Post already saved');
+      user.savedPosts.push(id);
     }
 
     await user.save();
-    res.status(200).send(user);
+    res.send(post);
   } catch (err) {
-    console.error('Error saving post:', err);
-    res.status(500).send('Error saving post');
+    res.status(500).send('Error liking/unliking post');
   }
 });
 
@@ -556,21 +559,41 @@ app.get('/current-user-following', authenticateToken, async (req, res) => {
 
 
 //return the uploaded post(personal-area)
-app.get('/uploaded-content', authenticateToken, async (req, res) => {
+app.get('/share', authenticateToken, async (req, res) => {
   try {
     const user = await User.findById(req.user.id);
     if (user) {
-      // Fetch posts where author matches the user's ID
-      const uploadedPosts = await Post.find({ author: user.id });
+      // Fetch posts where the user has shared them
+      const sharePosts = await Post.find({ 'shares.user': user.id }).populate('author', 'username firstName lastName');
 
-      res.json(uploadedPosts);
+      // Prepare array of shared posts with additional information
+      const userShares = [];
+      sharePosts.forEach(post => {
+        post.shares.forEach(share => {
+          if (share.user.toString() === user.id.toString()) {
+            userShares.push({
+              ...post.toObject(), // Convert Mongoose document to plain object
+              sharedText: share.text,
+              sharedAt: share.createdAt,
+              sharedBy: {
+                firstName: user.firstName,
+                lastName: user.lastName
+              }
+            });
+          }
+        });
+      });
+
+      res.json(userShares);
     } else {
       res.status(404).send('User not found');
     }
   } catch (error) {
+    console.error('Error fetching shared posts:', error);
     res.status(500).send(error.message);
   }
 });
+
 
 
 app.get('/uploaded-content', authenticateToken, async (req, res) => {
@@ -650,14 +673,6 @@ app.delete('/uploaded-content/:id', authenticateToken, async (req, res) => {
   // Handle removal of uploaded content
   res.send('Uploaded content removed');
 });
-
-
-
-
-
-
-
-
 
 
 
